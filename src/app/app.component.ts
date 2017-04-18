7e5c14076a7fceb4fb23845664a0a6c2c5a02102
Massimiliano Salerno <imafa82@gmail.com>
2017-04-18 20:49:56 +0200

    Primo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFire, FirebaseListObservable, AuthMethods, AuthProviders } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,107 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  cuisines: FirebaseListObservable<any[]>;
+  restaurants: Observable<any[]>;
+  exists;
+  displayName;
+  photoUrl;
+
+  constructor(private af: AngularFire){
+    console.log(af);
+   
+  }
+  ngOnInit(){
+      this.af.auth.subscribe(authState => {
+        if(!authState){
+          console.log("NOT LOGGED IN");
+          this.displayName= null;
+          this.photoUrl= null;
+        }
+          
+        else{
+          console.log("LOGGED IN", authState);
+          this.displayName = authState.auth.displayName;
+          this.photoUrl = authState.auth.photoURL;
+        }
+          
+      });
+     this.cuisines = this.af.database.list('/cuisines', {
+       query:{
+         orderByValue: true
+       }
+     });
+     this.restaurants = this.af.database.list('/restaurants', {
+       query:{
+         orderByChild: 'rating',
+         equalTo:5,
+         limitToLast: 50
+       }
+     })
+       .map(
+         restaurants => {
+           console.log("BEFORE MAP", restaurants);
+            restaurants.map(
+              restaurant =>{
+                restaurant.featureTypes=[];
+                for (var f in restaurant.features)
+                  restaurant.featureTypes.push(this.af.database.object('/features/'+f))
+                restaurant.cuisineType = this.af.database.object('/cuisines/' + restaurant.cuisine);
+                console.log(restaurant);
+              }
+            )
+           return restaurants;
+         }
+       );
+     this.exists = this.af.database.object('/restaurants/1/features/1');
+  }
+  
+  add(){
+    // this.cuisines.push({
+    //   name: 'Asian',
+    //   details:{
+    //     description: '...'
+    //   }
+    // });
+    this.af.database.list('/restaurants').push({name: ''})
+    .then(x=>{
+      console.log(x);
+      //x.key
+      let restaurant = { name: 'My New Restaurant'};
+      let update = {};
+      update['restaurants/'+x.key] = restaurant;
+      update['restaurants-by-city/camberwell/'+x.key] = restaurant;
+      this.af.database.object('/').update(update);
+    });
+  }
+
+  update(){
+    this.af.database.object('/restaurant').set(
+      {
+        name: 'new Name',
+        rating: 5
+
+      }
+    )
+  }
+
+  remove(){
+    this.af.database.object('/restaurant').remove()
+    .then(x=> console.log("SUCCESS"))
+    .catch(error => console.log("error", error));
+  }
+
+  login(){
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup
+    }).then(authState =>{
+      console.log("After login", authState);
+    });
+  }
+  
+  logout(){
+     this.af.auth.logout();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+export const firebaseConfig = {
+    apiKey: "AIzaSyAsD6qUZkS58eZsrpgBDDTFmqhkGaEQcpQ",
+    authDomain: "fir-5bd3f.firebaseapp.com",
+    databaseURL: "https://fir-5bd3f.firebaseio.com",
+    projectId: "fir-5bd3f",
+    storageBucket: "fir-5bd3f.appspot.com",
+    messagingSenderId: "248805438501"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
